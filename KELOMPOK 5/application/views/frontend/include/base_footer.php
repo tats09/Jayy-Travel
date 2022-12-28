02c60d46f874ea4b8edd371a210b3486c014817b
tats09 <114079763+tats09@users.noreply.github.com>
2022-12-29 02:15:56 +0700

    aa

---
+++ KELOMPOK 5/application/views/frontend/include/base_footer.php
@@ -0,0 +1,35 @@
+			<footer class="footer-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-7  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4 class="text-white">Tiket BUS</h4>
+								<p>
+									JAYY Travel adalah jasa pemesanan tiket bis secara online. JAYY Travel menawarkan pemesanan tiket bis melalui website, iOS dan aplikasi android untuk rute-rute utama berbagai kota di Indonesia.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4 class="text-white">Contact Us</h4>
+								<p>
+									JAYY Travel
+								</p>
+								<p class="number">
+									+62 11 22 33 44 55
+								</p>
+							</div>
+						</div>											
+					</div>
+					<div class="footer-bottom d-flex justify-content-between align-items-center flex-wrap">
+						<p class="footer-text m-0">Copyright &copy;2020 JAYY TRAVEL</p>
+					</div>
+				</div>
+			</footer>
+			<div class="preloader">
+				<div class="loading">
+					<img src="<?php echo base_url('assets/frontend/img/preloader.gif') ?>" width="80">
+					<p>Harap Tunggu</p>
+				</div>
+			</div>	
+			<!-- End footer Area -->
